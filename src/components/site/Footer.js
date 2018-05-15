04c593fe1a2f5b4e16420dc751f51e54fbe067ae
Marcia da Silva <marcia.dasilva@unijui.edu.br>
2018-05-15 17:06:09 -0300

    footer

--- src/components/site/Footer.js
+++ src/components/site/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 // import { SocialIcon } from 'react-social-icons';
 
 const Footer = props => (
@@ -13,7 +14,7 @@ const Footer = props => (
               href="https://www.facebook.com/tiago.pimentelbengochea"
               target="blank"
             >
-              <i className="fab fa-facebook" />
+              <i className="fab fa-facebook-square" />
             </a>
           </li>
           <li>
@@ -27,6 +28,31 @@ const Footer = props => (
           </li>
         </ul>
       </div>
+    </section>
+
+    <section className="footer__content">
+      <div className="footer__content--div">
+        <div className="footer__content--singlediv">
+          <h4>Sobre</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+             Molestias repellendus ea aliquam, perferendis similique autem adipisci aliquid et vitae. Suscipit!</p>
+        </div>
+        <div className="footer__content--singlediv"><h4>Contato</h4></div>
+        <div className="footer__content--singlediv"><h4>Ultimas postagens no Instagram</h4></div>
+        <div className="footer__content--singlediv"><h4>Ultima div</h4></div>
+      </div>
+    </section>
+
+    <section className="footer__devs">
+      <p>Desenvolvido por
+      <Link to="/">
+        <span> Marcia da Silva </span>
+      </Link>
+      e
+      <Link to="/">
+        <span> Igor Haugg </span>
+      </Link>
+       2018 &copy;</p>
     </section>
   </footer>
 );
