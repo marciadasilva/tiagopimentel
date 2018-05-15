05479e04a9d83cfd3bdb2be6e99d88b1904942fc
Igor Haugg <igor-haugg@hotmail.com>
2018-05-15 13:29:52 -0300

    social

--- src/components/site/Footer.js
+++ src/components/site/Footer.js
@@ -1,11 +1,32 @@
-import React from "react";
-import { SocialIcon } from "react-social-icons";
+import React from 'react';
+// import { SocialIcon } from 'react-social-icons';
 
 const Footer = props => (
   <footer className="footer">
-    <section className="social">
-      <SocialIcon url="https://www.facebook.com/tiago.pimentelbengochea" />
-      <SocialIcon url="https://www.instagram.com/pimenteltiago_/" />
+    <section className="social__wrapper">
+      <div className="social">
+        <p className="social__title">Redes Sociais</p>
+        <ul className="social__list">
+          <li>
+            <a
+              className="social__link"
+              href="https://www.facebook.com/tiago.pimentelbengochea"
+              target="blank"
+            >
+              <i className="fab fa-facebook" />
+            </a>
+          </li>
+          <li>
+            <a
+              className="social__link"
+              href="https://www.instagram.com/pimenteltiago_/"
+              target="blank"
+            >
+              <i className="fab fa-instagram" />
+            </a>
+          </li>
+        </ul>
+      </div>
     </section>
   </footer>
 );
