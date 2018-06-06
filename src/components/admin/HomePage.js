060d3e025dcadc92a30c55fc8aca0c1abc0de121
Igor Haugg <igor-haugg@hotmail.com>
2018-06-06 10:49:27 -0300

    messages alert on home page admin dashboard

--- src/components/admin/HomePage.js
+++ src/components/admin/HomePage.js
@@ -1,18 +1,43 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
 import MenuAdmin from './MenuAdmin';
+import selectMessages from '../../selectors/messages';
 
-const HomePage = () => (
-  <main className="dashboard">
-    <MenuAdmin />
-    <section className="dashboard__content">
-      <div className="dashboard__image-home" />
-      {/* <img
-        className="dashboard__image-home"
-        src="/images/dashboard.jpg"
-        alt="dashboard background"
-      /> */}
-    </section>
-  </main>
-);
+class HomePage extends React.Component {
+  render() {
+    const message =
+      this.props.messages.length > 1 ? 'novas mensagens' : 'nova mensagem';
+    console.log(message);
+    return (
+      <main className="dashboard">
+        <MenuAdmin />
+        <section className="dashboard__content">
+          <div className="dashboard__image-home">
+            {this.props.messages.length > 0 ? (
+              <div className="dashboard__card-alert">
+                <p>
+                  Você tem {this.props.messages.length} {message}!
+                </p>
+                <Link to="/admin/messages" className="button">
+                  Ver Mensagens
+                </Link>
+              </div>
+            ) : (
+              ''
+            )}
+          </div>
+        </section>
+      </main>
+    );
+  }
+}
 
-export default HomePage;
+const mapStateToProps = state => {
+  return {
+    messages: selectMessages(state.messages, state.filters)
+  };
+};
+
+export default connect(mapStateToProps)(HomePage);
