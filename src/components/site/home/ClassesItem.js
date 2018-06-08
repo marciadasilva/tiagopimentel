6059b4d638fc35bc27c3007abda71c6f302fb65e
Igor Haugg <igor-haugg@hotmail.com>
2018-06-08 12:39:35 -0300

    ajut

--- src/components/site/home/ClassesItem.js
+++ src/components/site/home/ClassesItem.js
@@ -1,17 +1,21 @@
-import React from "react";
-import ScrollAnimation from "react-animate-on-scroll";
-import { Link } from "react-router-dom";
+import React from 'react';
+import ScrollAnimation from 'react-animate-on-scroll';
+import { Link } from 'react-router-dom';
 
 const ClassesItem = props => {
   let backgroundImage = {
-    backgroundImage: "url(" + props.imgUrl + ")",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat"
+    backgroundImage: 'url(' + props.imgUrl + ')',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat'
   };
   return (
     <div className="classes__item" style={backgroundImage}>
-      <ScrollAnimation animateIn="fadeInUp" animateOnce={true}>
+      <ScrollAnimation
+        animateIn="fadeInUp"
+        animateOnce={true}
+        scrollableParentSelector=".classes-grid"
+      >
         <p className="classes__title">Aulas</p>
         <p className="classes__name">{props.name}</p>
         <p className="classes__description">{props.description}</p>
