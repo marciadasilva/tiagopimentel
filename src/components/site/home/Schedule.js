48a27136f330ef2717f8022d84e8ef0b692d0a55
Marcia da Silva <marcia.dasilva@unijui.edu.br>
2018-06-11 09:40:43 -0300

    template ie11

--- src/components/site/home/Schedule.js
+++ src/components/site/home/Schedule.js
@@ -13,6 +13,7 @@ const Schedule = () => {
             <div className="schedule__week">Quarta</div>
             {/* ordem por dia de semana: segunda, terça... */}
             {/* 1 linha */}
+            {/* segunda */}
             <div className="schedule__hour schedule__active">
               <em>Jiu-Jitsu</em>
               <p>18:00 - 19:15</p>
@@ -22,6 +23,7 @@ const Schedule = () => {
                 </Link>
               </div>
             </div>
+            {/* terça */}
             <div className="schedule__hour schedule__active">
               <em>Jiu-Jitsu</em>
               <p>18:00 - 19:15</p>
@@ -31,6 +33,7 @@ const Schedule = () => {
                 </Link>
               </div>
             </div>
+            {/* quarta */}
             <div className="schedule__hour schedule__active">
               <em>Jiu-Jitsu</em>
               <p>18:00 - 19:15</p>
@@ -41,6 +44,7 @@ const Schedule = () => {
               </div>
             </div>
             {/* 2 linha */}
+            {/* segunda */}
             <div className="schedule__hour schedule__active">
               <em>Muay Thai </em>
               <p>19:15 - 20:30</p>
@@ -50,7 +54,9 @@ const Schedule = () => {
                 </Link>
               </div>
             </div>
+            {/* terça */}
             <div className="schedule__hour" />
+            {/* quarta */}
             <div className="schedule__hour schedule__active">
               <em>Muay Thai </em>
               <p>19:15 - 20:30</p>
@@ -61,6 +67,7 @@ const Schedule = () => {
               </div>
             </div>
             {/* 3 linha */}
+            {/* segunda */}
             <div className="schedule__hour schedule__active">
               <em>Sogi</em>
               <p>20:30 - 21:30</p>
@@ -70,7 +77,9 @@ const Schedule = () => {
                 </Link>
               </div>
             </div>
+            {/* terça */}
             <div className="schedule__hour" />
+            {/* quarta */}
             <div className="schedule__hour schedule__active">
               <em>Sogi</em>
               <p>20:30 - 21:30</p>
@@ -80,14 +89,18 @@ const Schedule = () => {
                 </Link>
               </div>
             </div>
+            {/* segunda */}
             <div className="schedule__hour" />
+            {/* terça */}
             <div className="schedule__hour" />
+            {/* quarta */}
             <div className="schedule__hour" />
           </div>
           <div className="schedule__grid2">
             <div className="schedule__week">Quinta</div>
             <div className="schedule__week">Sexta</div>
             <div className="schedule__week">Sábado</div>
+            {/* quinta */}
             <div className="schedule__hour schedule__active">
               <em>Jiu-Jitsu</em>
               <p>18:00 - 19:15</p>
@@ -97,7 +110,9 @@ const Schedule = () => {
                 </Link>
               </div>
             </div>
+            {/* sexta */}
             <div className="schedule__hour" />
+            {/* sabado */}
             <div className="schedule__hour schedule__active">
               <em>Sogi</em>
               <p>14:00 - 15:15</p>
@@ -107,9 +122,12 @@ const Schedule = () => {
                 </Link>
               </div>
             </div>
-
+            {/* 2 linha */}
+            {/* quinta */}
             <div className="schedule__hour" />
+            {/* sexta */}
             <div className="schedule__hour" />
+            {/* sabado */}
             <div className="schedule__hour schedule__active">
               <em>Muay Thai </em>
               <p>15:20 - 16:30</p>
@@ -119,11 +137,21 @@ const Schedule = () => {
                 </Link>
               </div>
             </div>
+            {/* 3 linha */}
+            {/* quinta */}
             <div className="schedule__hour" />
+            {/* sexta */}
             <div className="schedule__hour" />
+            {/* sabado */}
+            <div className="schedule__hour schedule__active">
+              <em>Jiu-Jitsu </em>
+              <p>16:30 - 18:00</p>
+            </div>
+            {/* quinta */}
             <div className="schedule__hour" />
+            {/* sexta */}
             <div className="schedule__hour" />
-            <div className="schedule__hour" />
+            {/* sabado */}
             <div className="schedule__hour" />
           </div>
         </div>
